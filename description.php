44f7560bdf3729ec219e322079eff72edb91b99d
anassbt <anass.boumssimrat@gmail.com>
2017-09-11 14:53:14 +0200

    Adding two div (text / img) to description.php

--- description.php
+++ description.php
@@ -1,18 +1,56 @@
-<?php
+<body>
+  <?php
   include ("header.php");
   include ("prods.php");
 
   $enfant = $_POST['enfant'];
-  echo $enfant;
+
 
   ?>
-  <div class="childCard">
-    <figure>
-      <img src="<?php echo $prods[$enfant]['img']?>" alt="">
-      <h2>oui</h2>
-    </figure>
+  <main class="MainEnfant">
+    <div class="row description" >
 
-  </div>
-<?php
-include ("footer.php");
- ?>
+      <!-- description -->
+
+      <div class="col-sm-12 col-md-6 col-lg-6">
+
+
+        <h1>
+
+          <?php Echo $prods[$enfant]['Nom']." ". $prods[$enfant]['Prenom'];
+          ?>
+
+        </h1>
+
+        <h2>Caractéristques de l'enfant:</h2>
+
+        <ul>
+          <li>Nationalité: <?php Echo $prods[$enfant]['Pays'];
+
+          ?></li>
+          <li>Petit don de cet enfnant: <?php Echo $prods[$enfant]['Description'];
+
+          ?></li>
+
+        </ul>
+      </div>
+      
+
+
+      <!-- Photo de l'enfance -->
+
+      <div class="col-sm-12 col-md-6 col-lg-6">
+
+        <img src=" <?php Echo $prods[$enfant]['img']; ?>  " alt="">
+
+      </div>
+
+    </div>
+  </main>
+
+
+  <?php
+  include ("footer.php");
+  ?>
+
+</body>
