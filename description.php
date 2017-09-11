a148a99b433a9636a76300e54f8a3ec0ce39695f
remilefebvre <lefebvre.reminicolas@gmail.com>
2017-09-11 12:00:41 +0200

    add footer

--- description.php
+++ description.php
@@ -1,12 +1,18 @@
 <?php
+  include ("header.php");
   include ("prods.php");
 
   $enfant = $_POST['enfant'];
   echo $enfant;
 
   ?>
-
+  <div class="childCard">
     <figure>
       <img src="<?php echo $prods[$enfant]['img']?>" alt="">
       <h2>oui</h2>
     </figure>
+
+  </div>
+<?php
+include ("footer.php");
+ ?>
