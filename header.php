3f0a00f52fded480435247b372b9bb40b14ce10f
remilefebvre <lefebvre.reminicolas@gmail.com>
2017-09-15 14:30:52 +0200

    add headerbar

--- header.php
+++ header.php
@@ -1,3 +1,6 @@
+<?php session_start();
+  $_SESSION['connexion']=false;
+ ?>
 <!doctype html>
 <html class="no-js" lang="FR">
     <head>
@@ -29,6 +32,45 @@
     <?php include ("prods.php");?>
     <body>
       <header class="" id="header">
-            <h1 class=""><?php echo $info['h1'] ?></h1>
-            <input class="js-scrollTo" type="button" value="Prendre un Coeur >">
+
+        <nav id="navheader" class="navbar navbar-toggleable-md navbar-light bg-faded">
+          <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+
+          <h2><strong><a class="navbar-brand ml-1 navbar-brand" href="#">Le Plaisir d'Adopter</a></strong></h2>
+
+          <div class="collapse navbar-collapse " id="navbarText">
+            <ul class="navbar-nav mr-auto">
+              <?php
+              if ($_SESSION['connexion']==false) {
+                ?>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Connexion</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Créer un compte</a>
+              </li>
+              <?php
+            }
+            // SI non connecté
+            else {
+              ?>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Ajouter un enfant</a>
+              </li>
+              <?php
+              }
+              ?>
+            </ul>
+          </div>
+        </nav>
+
+
+
+
+
+
+        <h1 class=""><?php echo $info['h1'] ?></h1>
+        <input class="js-scrollTo" type="button" value="Prendre un Coeur >">
       </header>
